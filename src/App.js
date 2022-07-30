d6f2c5cd652bc2344e0a85c4f5b754d6ee1aaba9
Sudip Limbu <4slimbu@gmail.com>
2022-07-30 17:56:42 +0545

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,209 @@
-import logo from './logo.svg';
 import './App.css';
+import { rowA, rowB, weight, dataKeys, urbanData, ruralData } from './Data.js';
+
+// Plots data in the table
+const PlotData = ({ startIndex = 1, type, dataKeys, data, weight }) => {
+    return data.map((item, index) => {
+        let c = calculateC(dataKeys, item, weight);
+        return <tr key={index}>
+            <td>{index === 0 ? type : ''}</td>
+            <td>{startIndex++}</td>
+            <td>{item.hhsize}</td>
+            <td>{item.sc}</td>
+            <td>{item.at}</td>
+            <td>{item.n}</td>
+            <td>{item.m}</td>
+            <td>{item.e}</td>
+            <td>{item.s}</td>
+            <td>{item.w}</td>
+            <td>{item.f}</td>
+            <td>{item.c}</td>
+            <td>{item.a}</td>
+            <td>{c}</td>
+            <td>{c < 0.33 ? 'No' : 'Yes'}</td>
+            <td>{c < 0.33 ? 0 : c}</td>
+        </tr>
+    })
+
+}
+
+const parseWeight = (dataKeys, weight) => {
+    let w = [];
+
+    for (let i = 1; i < Object.keys(dataKeys).length; i++) {
+        w.push(eval(weight[dataKeys[i]]));
+    }
+
+    return w;
+}
+
+// Calculate c by using the formula: 
+const calculateC = (dataKeys, item, weight) => {
+    let c = 0;
+
+    for (let i = 1; i < Object.keys(dataKeys).length; i++) {
+        c += item[dataKeys[i]] * eval(weight[dataKeys[i]]);
+    }
+
+    return c.toFixed(2);
+}
+
+const calculateM0 = (dataKeys, data, weight) => {
+    // get total size
+    let totalSize = 0;
+    let hxck = 0;
+
+    data.forEach(item => {
+        let c = calculateC(dataKeys, item, weight);
+        let ck = c < 0.33 ? 0 : c;
+        hxck += item.hhsize * ck;
+        totalSize += item.hhsize;
+    })
+
+    return hxck / totalSize;
+}
+
+const percentageContribution = (dataKeys, data, weight) => {
+    let pc = [];
+    let m0 = calculateM0(dataKeys, data, weight);
+    let hc = headCount(dataKeys, data, weight, true);
+    let w = parseWeight(dataKeys, weight);
+    
+    hc.forEach((item, i) => {
+        let currentPC = item * w[i] / m0;
+
+        pc.push(currentPC.toFixed(2));
+    });
+
+    return pc;
+};
+
+const PercentageContribution = ({ dataKeys, data, weight}) => {
+    let pc = percentageContribution(dataKeys, data, weight);
+    return <>
+        {
+            pc.map(item => {
+                return <td>{item}</td>
+            })
+        }
+    </>;
+}
+
+const headCount = (dataKeys, data, weight, isCensored) => {
+    // get total size
+    let totalSize = 0;
+    data.forEach(item => {
+        totalSize += item.hhsize;
+    })
+
+    let headCount = [];
+    for (let i = 1; i < 11; i++) {
+        let currentHeadCount = 0;
+
+        data.forEach(item => {
+            let c = calculateC(dataKeys, item, weight);
+            if (isCensored && c < 0.33) {
+                return;
+            };
+
+            currentHeadCount += item.hhsize * item[dataKeys[i]];
+        });
+
+        headCount.push((currentHeadCount / totalSize).toFixed(2));
+    }
+
+    return headCount;
+}
+
+const HeadCount = ({ dataKeys, data, weight, isCensored = false}) => {
+    let hc = headCount(dataKeys, data, weight, isCensored);
+    return <>
+        {
+            hc.map(item => {
+                return <td>{item}</td>
+            })
+        }
+    </>;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="app-header">
+                <h1>The Deprivation Matrix</h1>
+            </header>
+            <div className="main">
+                <table>
+                    <tr>
+                        {
+                            rowA.map(item => {
+                                return <th>{item}</th>
+                            })
+                        }
+                    </tr>
+                    <tr>
+                        <th colSpan={3}></th>
+                        <th colSpan={2}>Education</th>
+                        <th colSpan={2}>Health</th>
+                        <th colSpan={6}>Standard of living</th>
+                    </tr>
+                    <tr>
+                        {
+                            rowB.map(item => {
+                                return <th>{item}</th>
+                            })
+                        }
+                    </tr>
+                    <PlotData
+                        type='Urban'
+                        dataKeys={dataKeys}
+                        data={urbanData}
+                        weight={weight}
+                    />
+                    <PlotData
+                        startIndex={Object.keys(urbanData).length + 1}
+                        type='Rural'
+                        dataKeys={dataKeys}
+                        data={ruralData}
+                        weight={weight}
+                    />
+                    <tr>
+                        <td colSpan={3}>Weight(w)</td>
+                        {
+                            dataKeys.slice(1).map(item => {
+                                return <td>{weight[item]}</td>
+                            })
+                        }
+                    </tr>
+                    <tr>
+                        <td colSpan={3}>Uncensored Headcount Ratio</td>
+                        <HeadCount
+                            dataKeys={dataKeys}
+                            data={urbanData.concat(ruralData)}
+                            weight={weight}
+                        />
+                    </tr>
+                    <tr>
+                        <td colSpan={3}>Censored Headcount Ratio</td>
+                        <HeadCount
+                            dataKeys={dataKeys}
+                            data={urbanData.concat(ruralData)}
+                            weight={weight}
+                            isCensored={true}
+                        />
+                    </tr>
+                    <tr>
+                        <td colSpan={3}>Percentage Contribution (in %)</td>
+                        <PercentageContribution
+                            dataKeys={dataKeys}
+                            data={urbanData.concat(ruralData)}
+                            weight={weight}
+                        />
+                    </tr>
+                </table>
+            </div>
+        </div >
+    );
 }
 
 export default App;
---
+++ src/Data.js
@@ -0,0 +1,14 @@
+export const rowA = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+export const rowB = ['Region', 'HH no.', 'HH Size', 'Sc', 'At', 'N', 'M', 'E', 'S', 'W', 'F', 'C', 'A', 'c', 'Poor', 'c(k)'];
+export const weight = {sc: '1/6', at: '1/6', n: '1/6', m: '1/6', e: '1/18', s: '1/18', w: '1/18', f: '1/18', c: '1/18', a: '1/18'};
+export const dataKeys = ['hhsize', 'sc', 'at', 'n', 'm', 'e', 's', 'w', 'f', 'c', 'a'];
+
+export const urbanData = [
+    { hhsize: 4, sc: 0, at: 0, n: 0, m: 1, e: 0, s: 0, w: 0, f: 0, c: 1, a: 0 },
+    { hhsize: 7, sc: 1, at: 1, n: 0, m: 1, e: 1, s: 0, w: 1, f: 0, c: 1, a: 1 },
+];
+
+export const ruralData = [
+    { hhsize: 5, sc: 0, at: 0, n: 1, m: 0, e: 1, s: 1, w: 1, f: 0, c: 1, a: 0 },
+    { hhsize: 4, sc: 1, at: 0, n: 0, m: 1, e: 1, s: 0, w: 0, f: 0, c: 1, a: 1 },
+];
