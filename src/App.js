5c7dfbed571e9cc20930f744fd5ab2c49a5e5f84
Sudip Limbu <4slimbu@gmail.com>
2022-08-28 15:19:31 +0545

    Add data filter

--- src/App.js
+++ src/App.js
@@ -1,32 +1,42 @@
+import { useState } from 'react';
 import './App.css';
 import { rowA, rowB, weight, dataKeys, urbanData, ruralData } from './Survey.js';
 
-//const simulatedUrbanData = urbanData.concat(urbanData).concat(urbanData);
-const simulatedUrbanData = urbanData;
-const simulatedRuralData = ruralData;
 
 // Plots data in the table
-const PlotData = ({ startIndex = 1, type, dataKeys, data, weight }) => {
+const PlotData = ({ startIndex = 1, type, dataKeys, data, weight, filter }) => {
+    let isTypeSet = 0;
+
     return data.map((item, index) => {
         let c = calculateC(dataKeys, item, weight);
-        return <tr key={index}>
-            <td>{index === 0 ? type : ''}</td>
-            <td>{startIndex++}</td>
-            <td>{item.hhsize}</td>
-            <td>{item.sc}</td>
-            <td>{item.at}</td>
-            <td>{item.n}</td>
-            <td>{item.m}</td>
-            <td>{item.e}</td>
-            <td>{item.s}</td>
-            <td>{item.w}</td>
-            <td>{item.f}</td>
-            <td>{item.c}</td>
-            <td>{item.a}</td>
-            <td>{c}</td>
-            <td>{c < 0.33 ? 'No' : 'Yes'}</td>
-            <td>{c < 0.33 ? 0 : c}</td>
-        </tr>
+
+        if (filter === 'poor' && c < 0.33) return;
+        if (filter === 'wealthy' && c >= 0.33) return;
+
+        const row = (
+            <tr key={index}>
+                <td>{isTypeSet ? '' : type}</td>
+                <td>{startIndex++}</td>
+                <td>{item.hhsize}</td>
+                <td>{item.sc}</td>
+                <td>{item.at}</td>
+                <td>{item.n}</td>
+                <td>{item.m}</td>
+                <td>{item.e}</td>
+                <td>{item.s}</td>
+                <td>{item.w}</td>
+                <td>{item.f}</td>
+                <td>{item.c}</td>
+                <td>{item.a}</td>
+                <td>{c}</td>
+                <td>{c < 0.33 ? 'No' : 'Yes'}</td>
+                <td>{c < 0.33 ? 0 : c}</td>
+            </tr>
+        );
+
+        if (!isTypeSet) isTypeSet = 1;
+
+        return row;
     })
 
 }
@@ -72,7 +82,7 @@ const percentageContribution = (dataKeys, data, weight) => {
     let m0 = calculateM0(dataKeys, data, weight);
     let hc = headCount(dataKeys, data, weight, true);
     let w = parseWeight(dataKeys, weight);
-    
+
     hc.forEach((item, i) => {
         let currentPC = item * w[i] / m0;
 
@@ -82,12 +92,12 @@ const percentageContribution = (dataKeys, data, weight) => {
     return pc;
 };
 
-const PercentageContribution = ({ dataKeys, data, weight}) => {
+const PercentageContribution = ({ dataKeys, data, weight }) => {
     let pc = percentageContribution(dataKeys, data, weight);
     return <>
         {
-            pc.map(item => {
-                return <td>{item}</td>
+            pc.map((item, index) => {
+                return <td key={item + index}>{item}</td>
             })
         }
     </>;
@@ -109,7 +119,7 @@ const mpiCalculation = (dataKeys, data, weight) => {
     return sumOfSizeXCk / totalSize;
 }
 
-const MPI = ({ dataKeys, data, weight}) => {
+const MPI = ({ dataKeys, data, weight }) => {
     let mpi = mpiCalculation(dataKeys, data, weight);
     return <td>{mpi.toFixed(2)}</td>;
 }
@@ -140,12 +150,12 @@ const headCount = (dataKeys, data, weight, isCensored) => {
     return headCount;
 }
 
-const HeadCount = ({ dataKeys, data, weight, isCensored = false}) => {
+const HeadCount = ({ dataKeys, data, weight, isCensored = false }) => {
     let hc = headCount(dataKeys, data, weight, isCensored);
     return <>
         {
-            hc.map(item => {
-                return <td>{item}</td>
+            hc.map((item, index) => {
+                return <td key={item + index}>{item}</td>
             })
         }
     </>;
@@ -168,7 +178,7 @@ const overallHeadCountCalculation = (dataKeys, data, weight) => {
     return sumOfSizeOfPoor / totalSize;
 }
 
-const OverallHeadCount = ({ dataKeys, data, weight}) => {
+const OverallHeadCount = ({ dataKeys, data, weight }) => {
     let ohc = overallHeadCountCalculation(dataKeys, data, weight);
     return <td>{ohc.toFixed(2)}</td>;
 }
@@ -180,109 +190,126 @@ const intensityCalculation = (dataKeys, data, weight) => {
     return mpi.toFixed(2) / overallHeadCount.toFixed(2);
 }
 
-const Intensity = ({ dataKeys, data, weight}) => {
+const Intensity = ({ dataKeys, data, weight }) => {
     let intensity = intensityCalculation(dataKeys, data, weight);
     return <td>{intensity.toFixed(2)}</td>;
 }
 
-function App() {
+const App = () => {
+    //const simulatedUrbanData = urbanData.concat(urbanData).concat(urbanData);
+    const simulatedUrbanData = urbanData;
+    const simulatedRuralData = ruralData;
+    const allData = simulatedUrbanData.concat(ruralData);
+    const ruralStartIndex = Object.keys(simulatedUrbanData).length + 1;
+    const [filter, setFilter] = useState('');
+
     return (
         <div className="App">
             <header className="app-header">
                 <h1>The Deprivation Matrix</h1>
             </header>
             <div className="main">
+                <div className="filter">
+                    <button className={filter === '' ? 'active' : ''} onClick={() => setFilter('')}>All</button>
+                    <button className={filter === 'poor' ? 'active' : ''} onClick={() => setFilter('poor')}>Poor</button>
+                    <button className={filter === 'wealthy' ? 'active' : ''} onClick={() => setFilter('wealthy')}>Wealthy</button>
+                </div>
                 <table>
-                    <tr>
-                        {
-                            rowA.map(item => {
-                                return <th>{item}</th>
-                            })
-                        }
-                    </tr>
-                    <tr>
-                        <th colSpan={3}></th>
-                        <th colSpan={2}>Education</th>
-                        <th colSpan={2}>Health</th>
-                        <th colSpan={6}>Standard of living</th>
-                    </tr>
-                    <tr>
-                        {
-                            rowB.map(item => {
-                                return <th>{item}</th>
-                            })
-                        }
-                    </tr>
-                    <PlotData
-                        type='Urban'
-                        dataKeys={dataKeys}
-                        data={simulatedUrbanData}
-                        weight={weight}
-                    />
-                    <PlotData
-                        startIndex={Object.keys(simulatedUrbanData).length + 1}
-                        type='Rural'
-                        dataKeys={dataKeys}
-                        data={simulatedRuralData}
-                        weight={weight}
-                    />
-                    <tr>
-                        <td colSpan={3}>Weight(w)</td>
-                        {
-                            dataKeys.slice(1).map(item => {
-                                return <td>{weight[item]}</td>
-                            })
-                        }
-                    </tr>
-                    <tr>
-                        <td colSpan={3}>Uncensored Headcount Ratio</td>
-                        <HeadCount
+                    <tbody>
+                        <tr>
+                            {
+                                rowA.map((item, index) => {
+                                    return <th key={item + index}>{item}</th>
+                                })
+                            }
+                        </tr>
+                        <tr>
+                            <th colSpan={3}></th>
+                            <th colSpan={2}>Education</th>
+                            <th colSpan={2}>Health</th>
+                            <th colSpan={6}>Standard of living</th>
+                        </tr>
+                        <tr>
+                            {
+                                rowB.map((item, index) => {
+                                    return <th key={item + index}>{item}</th>
+                                })
+                            }
+                        </tr>
+                        <PlotData
+                            type='Urban'
                             dataKeys={dataKeys}
-                            data={simulatedUrbanData.concat(simulatedRuralData)}
+                            data={simulatedUrbanData}
                             weight={weight}
+                            filter={filter}
                         />
-                    </tr>
-                    <tr>
-                        <td colSpan={3}>Censored Headcount Ratio</td>
-                        <HeadCount
+                        <PlotData
+                            startIndex={ruralStartIndex}
+                            type='Rural'
                             dataKeys={dataKeys}
-                            data={simulatedUrbanData.concat(simulatedRuralData)}
+                            data={simulatedRuralData}
                             weight={weight}
-                            isCensored={true}
+                            filter={filter}
                         />
-                    </tr>
-                    <tr>
-                        <td colSpan={3}>Percentage Contribution (in %)</td>
-                        <PercentageContribution
-                            dataKeys={dataKeys}
-                            data={simulatedUrbanData.concat(simulatedRuralData)}
-                            weight={weight}
-                        />
-                    </tr>
-                    <tr>
-                        <td colSpan={3}>MPI (in %)</td>
-                        <MPI
-                            dataKeys={dataKeys}
-                            data={simulatedUrbanData.concat(simulatedRuralData)}
-                            weight={weight}
-                        />
-                    </tr>
-                    <tr>
-                        <td colSpan={3}>Overall Headcount (in %)</td>
-                        <OverallHeadCount
-                            dataKeys={dataKeys}
-                            data={simulatedUrbanData.concat(simulatedRuralData)}
-                            weight={weight}
-                        />
-                    </tr>
-                    <tr>
-                        <td colSpan={3}>Intensity (in %)</td>
-                        <Intensity
-                            dataKeys={dataKeys}
-                            data={simulatedUrbanData.concat(simulatedRuralData)}
-                            weight={weight}
-                        />
-                    </tr>
+                        <tr>
+                            <td colSpan={3}>Weight(w)</td>
+                            {
+                                dataKeys.slice(1).map((item, index) => {
+                                    return <td key={item + index}>{weight[item]}</td>
+                                })
+                            }
+                        </tr>
+                        <tr>
+                            <td colSpan={3}>Uncensored Headcount Ratio</td>
+                            <HeadCount
+                                dataKeys={dataKeys}
+                                data={allData}
+                                weight={weight}
+                            />
+                        </tr>
+                        <tr>
+                            <td colSpan={3}>Censored Headcount Ratio</td>
+                            <HeadCount
+                                dataKeys={dataKeys}
+                                data={allData}
+                                weight={weight}
+                                isCensored={true}
+                            />
+                        </tr>
+                        <tr>
+                            <td colSpan={3}>Percentage Contribution (in %)</td>
+                            <PercentageContribution
+                                dataKeys={dataKeys}
+                                data={allData}
+                                weight={weight}
+                            />
+                        </tr>
+                        <tr>
+                            <td colSpan={3}>MPI (in %)</td>
+                            <MPI
+                                dataKeys={dataKeys}
+                                data={allData}
+                                weight={weight}
+                            />
+                        </tr>
+                        <tr>
+                            <td colSpan={3}>Overall Headcount (in %)</td>
+                            <OverallHeadCount
+                                dataKeys={dataKeys}
+                                data={allData}
+                                weight={weight}
+                            />
+                        </tr>
+                        <tr>
+                            <td colSpan={3}>Intensity (in %)</td>
+                            <Intensity
+                                dataKeys={dataKeys}
+                                data={allData}
+                                weight={weight}
+                            />
+                        </tr>
+
+                    </tbody>
                 </table>
             </div>
         </div >
